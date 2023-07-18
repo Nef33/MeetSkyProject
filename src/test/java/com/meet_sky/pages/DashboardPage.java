3427b74b57a98ec03eb26af42df3daba4485d896
Nefi33 <nefisekyrk@gmail.com>
2023-07-18 16:03:22 -0500

    Initial commit

---
+++ src/test/java/com/meet_sky/pages/DashboardPage.java
@@ -0,0 +1,27 @@
+package com.meet_sky.pages;
+
+
+
+
+import com.meet_sky.utilities.BrowserUtils;
+import com.meet_sky.utilities.ConfigurationReader;
+import com.meet_sky.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public abstract class BasePage {
+
+    public BasePage(){
+
+
+
+}}
---
+++ src/test/java/com/meet_sky/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,2 @@
+package com.meet_sky.step_definitions;public class LoginStepDefinitions {
+}
