f8faa1964902cca15cbdf392b84d6c8c3c4692c2
Electroman <nono.charaf@gmail.com>
2023-07-20 23:38:43 -0500

    create SearchFile stepDefinitions

---
+++ src/test/java/com/meet_sky/pages/SearchFileFunctionPage.java
@@ -0,0 +1,44 @@
+package com.meet_sky.pages;
+
+import com.meet_sky.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchFileFunctionPage {
+
+
+    public SearchFileFunctionPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+
+
+    @FindBy (xpath = "//a[@aria-label='Search']")
+    public WebElement SearchIcon;
+
+
+    @FindBy (xpath = "//input[@class='unified-search__form-input']")
+    public WebElement SearchBar;
+
+
+    @FindBy (xpath = "//span[@class='unified-search__result-content']")
+    public WebElement FileIcon;
+
+
+    @FindBy (xpath = "//*[@id=\"app-sidebar-vue\"]/header/div[1]/div[2]")
+    public WebElement SideBarIcon;
+
+
+    @FindBy(xpath = "//*[@id=\"viewer\"]/div[1]/div[2]/button[2]/span[1]")
+    public WebElement closeIcon;
+
+
+
+
+
+
+
+}
